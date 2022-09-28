12cf5131a1d9292e8a473dd171cccafbcdc87833
Asylbekkk <arakhmanberdy@gmail.com>
2022-09-28 14:06:53 +0600

    Second commit

--- lab2.py
+++ lab2.py
@@ -1,7 +1,7 @@
-radius = int(input("Введите радиус: "))
+radius = int(input("радиус жаз "))
 
 if radius >= 0:
     print("Длина окружности = ",  2  *  3.14  *  radius)
-    print("Площадь = ", 3.14 * radius ** 2)
+    print("Аудан = ", 3.14 * radius ** 2)
 else:
     print("Пожалуйста, введите положительное число")
